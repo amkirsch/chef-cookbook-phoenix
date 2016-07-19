9dae7be89e73a85deee9f8fab4f20bc5ce5522a9
Andrew Kirschke <amkirsch@us.ibm.com>
2016-07-19 13:13:07 -0400

    Made changes to Metadata
    - Added changes to support upload to chef supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,13 @@
 name 'phoenix'
 maintainer 'Andrew M. Kirschke'
 maintainer_email 'andrew.kirschke@gmx.com'
+issues_url 'https://github.com/amkirsch/chef-cookbook-phoenix/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/amkirsch/chef-cookbook-phoenix' if respond_to?(:source_url)
 license 'all_rights'
-description 'Installs/Configures Phoenix'
-long_description 'Installs/Configures Phoenix'
-version '0.2.0'
+description 'Installs and Configures Phoenix'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '1.2.0'
+
+supports 'redhat'
+supports 'fedora'
+supports 'centos'
